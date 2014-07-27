cbe2097916ac575cbc24c1776e595ed40cc02fe3
pozharko <canoneer@mail.ru>
2014-07-27 19:20:44 +0400

    Comments added

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -6,13 +6,18 @@ import play.api.mvc._
 import models.YandexRequest
 
 /**
- * Created by Dmitry Meshkov on 24.07.2014.
+ * @author Dmitry Meshkov
+ * @since 24.07.2014
  */
 object Search extends Controller {
+  /**
+   * Request Yandex Blogs search for keywords in the query list
+   * and show second level domain frequency
+   */
   def search(query: List[String]) = Action {
-    val stats  = new YandexRequest(query).stats
-    val statsJs = stats map(x => x._1 -> JsNumber(x._2))
-    val json = JsObject(statsJs.toSet.toSeq.sortWith(_._1 < _._1))
+    val stats = new YandexRequest(query).stats
+    val statsJs = stats map (x => x._1 -> JsNumber(x._2))
+    val json = JsObject(statsJs.toSeq.sortWith(_._1 < _._1))
 
     Ok(Json.prettyPrint(json)).as(ContentTypes.JSON)
   }
--- app/models/YandexRequest.scala
+++ app/models/YandexRequest.scala
@@ -13,21 +13,45 @@ import java.net.URL
  * @since 24.07.2014
  */
 class YandexRequest(query: List[String]) {
+  /**
+   * Wait response for 10 seconds
+   */
   private val awaitDuration = Duration(10, "s")
+  /**
+   * Request Yandex in 10 threads
+   */
   implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
 
+  /**
+   * Urls to request
+   */
   private val urls = query map ("http://blogs.yandex.ru/search.rss?text=" + _)
+  /**
+   * Request pool.
+   * Takes first 10 links from each request
+   */
   private val futureRequests = urls map (url => Future {
     (XML.load(url) \\ "item").map(x => (x \\ "link").text).toSet.take(10)
   })
+  /**
+   * Aggregated request
+   */
   private val aggregated = Future.sequence(futureRequests)
+  /**
+   * Links from all requests
+   */
   private lazy val links = Await.result(aggregated, awaitDuration).flatten.distinct
-
+  /**
+   * Second level domains from links
+   */
   private lazy val domains: List[String] = links map ({ x =>
     val host = new URL(x).getHost
     val domainName = InternetDomainName.from(host)
     domainName.topPrivateDomain().toString
   })
+  /**
+   * Domains frequency statistics
+   */
   lazy val stats = domains map ((x: String) => (x, domains.count(_ == x))) toSet
 
 }
